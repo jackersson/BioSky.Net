51beaee8b8beb8536c5ef8807d3d50502775ac39
AlexandrSpark <adaman1991@gmail.com>
2016-03-15 20:28:40 +0200

    Add LoginPopup with AuthenticationWindow

---
+++ BioSky.Net/BioModule/ViewModels/LoginPopupViewModel.cs
@@ -0,0 +1,35 @@
+﻿using BioContracts;
+using Caliburn.Micro;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BioModule.ViewModels
+{
+  public class LoginPopupViewModel : Screen
+  {    
+    public void Show()
+    {      
+      IsPopupOpen = true;   
+    }
+
+    public void Hide()
+    {      
+      IsPopupOpen = false;
+    }    
+
+    private bool _isPopupOpen;
+    public bool IsPopupOpen
+    {
+      get { return _isPopupOpen; }
+      set
+      {
+        if (_isPopupOpen != value)
+        {
+          _isPopupOpen = value;
+          NotifyOfPropertyChange(() => IsPopupOpen);
+        }
+      }
+    }    
+  }
+}
