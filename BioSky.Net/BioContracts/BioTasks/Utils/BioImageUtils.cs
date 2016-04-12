75c47e302066ffd04d55e71553adab9139dd6936
Taras Lishchenko <jackersson@gmail.com>
2016-04-12 11:52:12 +0300

    Included changes

---
+++ BioSky.Net/BioContracts/BioTasks/Utils/BioImageUtils.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace BioContracts.BioTasks.Utils
+{
+  public class BioImageUtils
+  {
+    public byte[] ImageToByte(Image img)
+    {
+      byte[] byteArray = new byte[0];
+      using (MemoryStream stream = new MemoryStream())
+      {
+        img.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+        stream.Close();
+
+
+        byteArray = stream.ToArray();
+      }
+      return byteArray;
+    }
+
+    public byte[] ImageToByte(Stream stream)
+    {
+
+      byte[] buffer = null;
+      if (stream != null && stream.Length > 0)
+      {
+        using (BinaryReader br = new BinaryReader(stream))
+        {
+          buffer = br.ReadBytes((int)stream.Length);
+        }
+      }
+      return buffer;
+    }
+
+    public Google.Protobuf.ByteString ImageToByteString(Image img)
+    {
+      byte[] bytes = ImageToByte(img);
+      return Google.Protobuf.ByteString.CopyFrom(bytes);
+    }
+
+    public Google.Protobuf.ByteString ImageToByteString(Stream img)
+    {
+      byte[] bytes = ImageToByte(img);
+      return Google.Protobuf.ByteString.CopyFrom(bytes);
+    }
+
+  }
+}
---
+++ BioSky.Net/BioContracts/Locations/TrackLocation.cs
@@ -0,0 +1,271 @@
+﻿using BioContracts.Common;
+using BioContracts.Services;
+using BioService;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using AForge.Video.DirectShow;
+using BioContracts.CaptureDevices;
+using BioContracts.Locations;
+using BioContracts.Locations.BioTasks;
+using BioContracts.BioTasks;
+using BioContracts.Locations.Observers;
+using BioContracts.AccessDevices;
+
+namespace BioContracts
+{
+  
+  public class TrackLocation : PropertyChangedBase, IBioObservable<IFullLocationObserver>
+                             , ICaptureDeviceObserver, IAccessDeviceObserver, IVerificationObserver
+  {   
+    public TrackLocation(IProcessorLocator locator, Location location)
+    {
+      _locator = locator;
+     
+      _database            = locator.GetProcessor<IBioSkyNetRepository>();     
+      _bioService          = locator.GetProcessor<IServiceManager>().DatabaseService;
+      _notifier            = locator.GetProcessor<INotifier>();
+      _accessDeviceEngine  = locator.GetProcessor<IAccessDeviceEngine>();
+      _captureDeviceEngine = locator.GetProcessor<ICaptureDeviceEngine>();
+
+      //_veryfier = new Verifyer(_locator);
+      _observer = new BioObserver<IFullLocationObserver>();
+      _devices  = new Dictionary<LocationDevice, ILocationDeviceObserver>();
+      _verifyer = new TrackLocationVerification(locator);
+
+      _verifyer.Subscribe(this);
+
+       Update(location);
+    }
+
+    public void Update(Location location)
+    {
+      if (location == null)
+        return;
+      _previousLocation = _currentLocation;
+
+      _currentLocation  = location;
+      Start();
+    }
+
+    private void Start()
+    {
+      Init();
+
+      foreach (KeyValuePair<LocationDevice, ILocationDeviceObserver> pair in _devices)
+        pair.Value.Start(_currentLocation); 
+    }
+
+    private void Init()
+    {
+      if (_currentLocation == null)
+        return;
+
+      if (_currentLocation.AccessDevice != null)
+      {
+        if (!_devices.ContainsKey(LocationDevice.AccessDevice))
+          _devices.Add(LocationDevice.AccessDevice, new LocationAccessDeviceObserver(_locator, this));         
+      }
+
+      if (_currentLocation.CaptureDevice != null)
+      {
+        if (!_devices.ContainsKey(LocationDevice.CaptureDevice))
+          _devices.Add(LocationDevice.CaptureDevice, new LocationCaptureDeviceObserver(_locator, this));        
+      }
+    }
+    
+    private bool IsOk()
+    {     
+      foreach ( KeyValuePair<LocationDevice, ILocationDeviceObserver> par in _devices)
+      {
+       bool state = par.Value.IsDeviceOk;
+       if (!state)
+          return state;
+      }
+
+      return _devices.Count > 0 ? true : false;
+    }
+
+    public void Subscribe(IFullLocationObserver observer) {
+      _observer.Subscribe(observer);
+    }
+
+    public void Unsubscribe(IFullLocationObserver observer){
+      _observer.Unsubscribe(observer);
+    }
+
+    public void UnsubscribeAll() {
+      _observer.UnsubscribeAll();
+    }
+
+    public void OnFrame(ref Bitmap frame) {
+      foreach (KeyValuePair<int, IFullLocationObserver> observer in _observer.Observers)
+        observer.Value.OnCaptureDeviceFrameChanged(ref frame);
+    }
+
+    public void OnStop(bool stopped, string message) {
+      if (_observer.Observers.Count <= 0)
+        return;    
+      OnError(new Exception(message));
+    }
+
+    public void OnStart(bool started, VideoCapabilities active, VideoCapabilities[] all) {
+      OnReady(started);
+    }
+
+    public void OnCardDetected(string cardNumber){
+      _verifyer.StartByCard(cardNumber, CurrentLocation);
+      //foreach (KeyValuePair<int, IFullLocationObserver> observer in _observer.Observers)
+      //  observer.Value.OnStartVerificationByCard(cardNumber);    
+    }
+
+    public void OnError(Exception ex){
+      foreach (KeyValuePair<int, IFullLocationObserver> observer in _observer.Observers)
+        observer.Value.OnError(ex);
+    }
+
+    public void OnReady(bool isReady){
+      foreach (KeyValuePair<int, IFullLocationObserver> observer in _observer.Observers)
+        observer.Value.OnOk(IsOk());       
+    }
+
+    public string GetAccessDeviceName {
+      get { return _currentLocation.AccessDevice == null ? "" : _currentLocation.AccessDevice.Portname; }
+    }
+    public string GetCaptureDeviceName {
+      get { return _currentLocation.CaptureDevice == null ? "" : _currentLocation.CaptureDevice.Devicename; }
+    }
+
+    public void Stop()
+    {
+      foreach (KeyValuePair<LocationDevice, ILocationDeviceObserver> pair in _devices)
+        pair.Value.Stop();
+    }
+
+    public void OnVerificationFailure(Exception ex)
+    {
+      foreach (KeyValuePair<int, IFullLocationObserver> observer in _observer.Observers)
+        observer.Value.OnVerificationFailure(ex);
+
+      _accessDeviceEngine.Execute(AccessDeviceCommands.CommandReady, GetAccessDeviceName);
+    }
+
+    public void OnVerificationSuccess(bool state)
+    {
+      foreach (KeyValuePair<int, IFullLocationObserver> observer in _observer.Observers)
+        observer.Value.OnVerificationSuccess(state);
+
+      _accessDeviceEngine.Execute(AccessDeviceCommands.CommandAccess, GetAccessDeviceName);
+
+      _accessDeviceEngine.Execute(AccessDeviceCommands.CommandReady, GetAccessDeviceName);
+    }
+
+    public void OnVerificationProgress(int progress)
+    {
+      foreach (KeyValuePair<int, IFullLocationObserver> observer in _observer.Observers)
+        observer.Value.OnVerificationProgress( progress);    
+    }
+
+    /*
+
+       private async void OnVisitorVerified(Photo photo, Person person)
+       {
+         try
+         {
+           _veryfier.VerificationDone -= OnVisitorVerified;
+
+           bool status = (photo != null && person != null && photo.Personid == person.Id);
+           _visitor.Personid = status ? person.Id : 0;
+
+           if (photo != null)        
+             _visitor.Personid = photo.Personid;       
+
+           _visitor.Status = status ? Result.Success : Result.Failed;
+
+           await _bioService.VisitorDataClient.Add(_visitor);
+         }
+         catch (Exception e)
+         {
+           _notifier.Notify(e);
+         }
+       }    
+      */
+
+    public IScreen ScreenViewModel { get; set; }
+
+    public string Caption {
+      get { return _currentLocation.LocationName; }
+    }
+
+    public Location CurrentLocation {
+      get { return _currentLocation; }
+    }
+
+    /*
+    public void OnCardDetected(string cardNumber)
+    {      
+      if (_veryfier.Busy)
+        return;
+
+      _visitor = null;
+
+      Person person = _database.Persons.GetPersonByCardNumber(cardNumber);
+
+      _visitor = new Visitor() { Locationid   = _currentLocation.Id                                      
+                               , Time         = DateTime.Now.Ticks                                           
+                               , CardNumber   = cardNumber   };
+
+
+      if (person != null)      
+        _visitor.Personid = person.Id;            
+
+     // _veryfier.VerificationDone -= OnVisitorVerified;
+     // _veryfier.VerificationDone += OnVisitorVerified; 
+
+      //TODO check on NULL
+     // _veryfier.Start(CurrentLocation.CaptureDevice.Devicename, person);     
+    }  
+    */
+   
+    #region UI   
+   
+    #endregion
+
+    #region Global Variables  
+   // private Verifyer _veryfier;
+    private Location _currentLocation ;
+    private Location _previousLocation;
+    private BioObserver<IFullLocationObserver>    _observer;
+    private TrackLocationVerification _verifyer;
+    //private Visitor  _visitor  ;
+
+    private Dictionary<LocationDevice, ILocationDeviceObserver> _devices;
+    
+    private readonly IDatabaseService     _bioService         ;
+    private readonly IBioSkyNetRepository _database           ;
+    private readonly IProcessorLocator    _locator            ;
+    private readonly INotifier            _notifier           ;
+    private readonly IAccessDeviceEngine  _accessDeviceEngine ;
+    private readonly ICaptureDeviceEngine _captureDeviceEngine;
+    #endregion
+  }
+
+  public class DeviceStateHolder
+  {
+    public bool IsOK { get; private set; }
+    string Message { get; set; }
+
+    public void Update(bool state, string message = "")
+    {
+      IsOK = state;
+      Message = message;
+    }
+
+    public void Reset()
+    {
+      IsOK = false;
+      Message = "";
+    }
+  }
+}
